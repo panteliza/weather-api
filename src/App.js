267dff6b068ed651a5142b65518d736fbf2f43bb
Eliza Pant <elizapant.github@gmail.com>
2023-06-11 20:20:36 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState("");
+  const [post, setPost] = useState({});
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&appid=7985b400513334a733ef849fe7be1121`;
+
+  function Clickgarne() {
+    axios.get(url).then((res) => {
+      setPost(res.data);
+    });
+  }
+
+  function Changegarne(event) {
+    setValue(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-gray-700'>
+      <div className="flex flex-col items-center justify-center gap-[50px] pt-[70px]">
+        <div className='text-white text-[40px] font-bold'>Weather App</div>
+        <div className="flex gap-[3px]">
+          <input
+            type='text'
+            placeholder='Enter City'
+            onChange={Changegarne}
+            className='border border-blue-700'
+          />
+          <button className='px-[6px] py-[3px] bg-green-600' onClick={Clickgarne}>
+            Search
+          </button>
+        </div>
+
+        {typeof post.main !== "undefined" ? (
+          <div>
+            <div className="text-[20px] text-white pt-[20px]">{post.name}</div>
+            <div className="text-[20px] text-white pt-[20px]">{post.main.temp}</div>
+            <div className="text-[20px] text-white pt-[20px]">{post.weather[0].main}</div>
+            <div className="text-[20px] text-white pt-[20px]">
+              ({post.weather[0].description})
+            </div>
+          </div>
+        ) : (
+          null
+        )}
+      </div>
     </div>
   );
 }
